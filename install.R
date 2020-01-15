afc9e26ed32a5c31cff7356301af02b6d8c47c76
Martin Stingl <martin.stingl@dfv.de>
2020-01-15 16:37:08 +0100

    support external packages

--- install.R
+++ install.R
@@ -1,7 +1,9 @@
-# Basics
-install.packages(c("tidyverse", "lubridate"))
+# # Basics
+# install.packages(c("tidyverse", "lubridate"))
 
 # Shiny
 install.packages("shiny")
 
+# external packages
+install.packages("remotes")
 remotes::install_github("dfv-ms/PCArt")
